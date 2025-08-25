e84d64ca1b6f9c1374277d0960164bca56536a70
Jack Wright <56345+ayax79@users.noreply.github.com>
2025-08-25 13:19:38 -0700

    Reenable copilot (#3931)
    Signed-off-by: Jack Wright <jack.wright@nike.com>
    Co-authored-by: Jack Wright <jack.wright@nike.com>

--- crates/goose/src/providers/factory.rs
+++ crates/goose/src/providers/factory.rs
@@ -10,6 +10,7 @@ use super::{
     databricks::DatabricksProvider,
     gcpvertexai::GcpVertexAIProvider,
     gemini_cli::GeminiCliProvider,
+    githubcopilot::GithubCopilotProvider,
     google::GoogleProvider,
     groq::GroqProvider,
     lead_worker::LeadWorkerProvider,
@@ -48,6 +49,7 @@ static REGISTRY: Lazy<RwLock<ProviderRegistry>> = Lazy::new(|| {
         registry.register::<DatabricksProvider, _>(DatabricksProvider::from_env);
         registry.register::<GcpVertexAIProvider, _>(GcpVertexAIProvider::from_env);
         registry.register::<GeminiCliProvider, _>(GeminiCliProvider::from_env);
+        registry.register::<GithubCopilotProvider, _>(GithubCopilotProvider::from_env);
         registry.register::<GoogleProvider, _>(GoogleProvider::from_env);
         registry.register::<GroqProvider, _>(GroqProvider::from_env);
         registry.register::<LiteLLMProvider, _>(LiteLLMProvider::from_env);
